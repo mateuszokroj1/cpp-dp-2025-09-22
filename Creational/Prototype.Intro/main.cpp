00b6bf95caf80df0904ee38fac42a8f2c4b9f6f9
Krystian Piekos <krystian.piekos@infotraining.pl>
2025-09-23 09:31:29 +0200

    Prototype with Clone()

--- Creational/Prototype.Intro/main.cpp
+++ Creational/Prototype.Intro/main.cpp
@@ -8,48 +8,64 @@ class Engine
 public:
     virtual void start() = 0;
     virtual void stop() = 0;
+    virtual std::unique_ptr<Engine> clone() const = 0;
     virtual ~Engine() = default;
 };
 
 class Diesel : public Engine
 {
 public:
-    virtual void start() override
+    void start() override
     {
         std::cout << "Diesel starts\n";
     }
 
-    virtual void stop() override
+    void stop() override
     {
         std::cout << "Diesel stops\n";
+    }
+
+    std::unique_ptr<Engine> clone() const override
+    {
+        return std::make_unique<Diesel>(*this);
     }
 };
 
 class TDI : public Diesel
 {
 public:
-    virtual void start() override
+    void start() override
     {
         std::cout << "TDI starts\n";
     }
 
-    virtual void stop() override
+    void stop() override
     {
         std::cout << "TDI stops\n";
+    }
+
+    std::unique_ptr<Engine> clone() const override
+    {
+        return std::make_unique<TDI>(*this);
     }
 };
 
 class Hybrid : public Engine
 {
 public:
-    virtual void start() override
+    void start() override
     {
         std::cout << "Hybrid starts\n";
     }
 
-    virtual void stop() override
+    void stop() override
     {
         std::cout << "Hybrid stops\n";
+    }
+
+    std::unique_ptr<Engine> clone() const override
+    {
+        return std::make_unique<Hybrid>(*this);
     }
 };
 
@@ -64,16 +80,8 @@ public:
     }
 
     Car(const Car& other)
-        : engine_{}
+        : engine_{other.engine_->clone()}
     {
-        if (typeid(*other.engine_) == typeid(Diesel))
-            engine_ = std::make_unique<Diesel>();
-        else if (typeid(*other.engine_) == typeid(TDI))
-            engine_ = std::make_unique<TDI>();
-        else if (typeid(*other.engine_) == typeid(Hybrid))
-            engine_ = std::make_unique<Hybrid>();
-        else
-            assert(false && "Unknown engine type");
     }
 
     void drive(int km)
@@ -86,10 +94,11 @@ public:
 
 int main()
 {
-    Car c1{std::make_unique<Hybrid>()};
+    Car c1{std::make_unique<TDI>()};
     c1.drive(100);
 
     std::cout << "\n";
 
     Car c2 = c1;
+    c2.drive(200);
 }
