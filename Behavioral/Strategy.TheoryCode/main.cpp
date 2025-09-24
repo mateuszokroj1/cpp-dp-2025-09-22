161193dca324d741bdd0e95db6bc831e1137843c
Krystian Piekos <krystian.piekos@infotraining.pl>
2025-09-24 10:38:10 +0200

    Strategy

--- Behavioral/Strategy.TheoryCode/main.cpp
+++ Behavioral/Strategy.TheoryCode/main.cpp
@@ -1,14 +1,31 @@
 #include "strategy.hpp"
 
-int main()
+void canonical_usage_of_strategy()
 {
-    DataContext data{std::make_shared<UpperCaseFormatter>()};
+    using namespace Canonical;
+
+    Document data{std::make_shared<UpperCaseFormatter>()};
     data.pretty_print();
 
     auto lcf = std::make_shared<LowerCaseFormatter>();
-    data.reset_formatter(lcf);
+    data.set_formatter(lcf);
     data.pretty_print();
 
-    data.reset_formatter(std::make_shared<CapitalizeFormatter>());
+    data.set_formatter(std::make_shared<CapitalizeFormatter>());
+    data.pretty_print();
+}
+
+int main()
+{
+    using namespace ModernCpp;
+
+    Document data{UpperCaseFormatter{}};
+    data.pretty_print();
+
+    LowerCaseFormatter lcf;
+    data.set_formatter(lcf);
+    data.pretty_print();
+
+    data.set_formatter(CapitalizeFormatter{});
     data.pretty_print();
 }
--- Behavioral/Strategy.TheoryCode/strategy.hpp
+++ Behavioral/Strategy.TheoryCode/strategy.hpp
@@ -2,98 +2,188 @@
 #define STRATEGY_HPP_
 
 #include <algorithm>
+#include <cctype>
 #include <cstring>
 #include <functional>
 #include <iostream>
 #include <memory>
-#include <cctype>
 #include <string>
 
-// "Strategy"
-class Formatter
+namespace Canonical
 {
-public:
-    virtual std::string format(const std::string& data) = 0;
-    virtual ~Formatter() = default;
-};
-
-// "ConcreteStrategyA"
-class UpperCaseFormatter : public Formatter
-{
-public:
-    std::string format(const std::string& data) override
+    // "Strategy"
+    class Formatter
     {
-        std::string transformed_data{data};
+    public:
+        virtual std::string format(const std::string& data) = 0;
+        virtual ~Formatter() = default;
+    };
 
-        std::transform(data.begin(), data.end(), transformed_data.begin(), [](char c) { return std::toupper(c); });
-
-        return transformed_data;
-    }
-};
-
-// "ConcreteStrategyB"
-class LowerCaseFormatter : public Formatter
-{
-public:
-    std::string format(const std::string& data) override
+    // "ConcreteStrategyA"
+    class UpperCaseFormatter : public Formatter
     {
-        std::string transformed_data{data};
-
-        std::transform(data.begin(), data.end(), transformed_data.begin(), [](char c) { return std::tolower(c); });
-
-        return transformed_data;
-    }
-};
-
-// "ConcreteStrategyC"
-class CapitalizeFormatter : public Formatter
-{
-public:
-    std::string format(const std::string& data) override
-    {
-        std::string transformed_data{data};
-
-        if (data.size() >= 1)
+    public:
+        std::string format(const std::string& data) override
         {
-            transformed_data[0] = static_cast<char>(std::toupper(data.front()));
+            std::string transformed_data{data};
 
-            std::transform(data.begin() + 1, data.end(), transformed_data.begin() + 1, [](char c) { return std::tolower(c); });
+            std::transform(data.begin(), data.end(), transformed_data.begin(), [](char c) { return std::toupper(c); });
+
+            return transformed_data;
+        }
+    };
+
+    // "ConcreteStrategyB"
+    class LowerCaseFormatter : public Formatter
+    {
+    public:
+        std::string format(const std::string& data) override
+        {
+            std::string transformed_data{data};
+
+            std::transform(data.begin(), data.end(), transformed_data.begin(), [](char c) { return std::tolower(c); });
+
+            return transformed_data;
+        }
+    };
+
+    // "ConcreteStrategyC"
+    class CapitalizeFormatter : public Formatter
+    {
+    public:
+        std::string format(const std::string& data) override
+        {
+            std::string transformed_data{data};
+
+            if (data.size() >= 1)
+            {
+                transformed_data[0] = static_cast<char>(std::toupper(data.front()));
+
+                std::transform(data.begin() + 1, data.end(), transformed_data.begin() + 1, [](char c) { return std::tolower(c); });
+            }
+
+            return transformed_data;
+        }
+    };
+
+    // "Context"
+    class Document
+    {
+        std::shared_ptr<Formatter> strategy_;
+        std::string data_ = "text";
+
+    public:
+        Document(std::shared_ptr<Formatter> strategy)
+            : strategy_{strategy}
+        {
         }
 
-        return transformed_data;
-    }
-};
+        void set_formatter(std::shared_ptr<Formatter> new_strategy)
+        {
+            strategy_ = new_strategy;
+        }
 
-// "Context"
-class DataContext
+        void pretty_print()
+        {
+            std::cout << "Text: " << strategy_->format(data_) << std::endl;
+        }
+
+        std::string data() const
+        {
+            return data_;
+        }
+
+        void set_data(const std::string& data)
+        {
+            data_ = data;
+        }
+    };
+} // namespace Canonical
+
+namespace ModernCpp
 {
-    std::shared_ptr<Formatter> strategy_;
-    std::string data_ = "text";
+    // "Strategy"
+    using Formatter = std::function<std::string(const std::string&)>;
 
-public:
-    DataContext(std::shared_ptr<Formatter> strategy) : strategy_{strategy}
+    // "ConcreteStrategyA"
+    class UpperCaseFormatter
     {
-    }
+    public:
+        std::string operator()(const std::string& data)
+        {
+            std::string transformed_data{data};
 
-    void reset_formatter(std::shared_ptr<Formatter> new_strategy)
-    {
-        strategy_ = new_strategy;
-    }
+            std::transform(data.begin(), data.end(), transformed_data.begin(), [](char c) { return std::toupper(c); });
 
-    void pretty_print()
-    {
-        std::cout << "Data: " << strategy_->format(data_) << std::endl;
-    }
+            return transformed_data;
+        }
+    };
 
-    std::string data() const
+    // "ConcreteStrategyB"
+    class LowerCaseFormatter
     {
-        return data_;
-    }
+    public:
+        std::string operator()(const std::string& data)
+        {
+            std::string transformed_data{data};
 
-    void set_data(const std::string& data)
+            std::transform(data.begin(), data.end(), transformed_data.begin(), [](char c) { return std::tolower(c); });
+
+            return transformed_data;
+        }
+    };
+
+    // "ConcreteStrategyC"
+    class CapitalizeFormatter
     {
-        data_ = data;
-    }
-};
+    public:
+        std::string operator()(const std::string& data)
+        {
+            std::string transformed_data{data};
+
+            if (data.size() >= 1)
+            {
+                transformed_data[0] = static_cast<char>(std::toupper(data.front()));
+
+                std::transform(data.begin() + 1, data.end(), transformed_data.begin() + 1, [](char c) { return std::tolower(c); });
+            }
+
+            return transformed_data;
+        }
+    };
+
+    // "Context"
+    class Document
+    {
+        Formatter formatter_;
+        std::string data_ = "text";
+
+    public:
+        Document(Formatter strategy)
+            : formatter_{strategy}
+        {
+        }
+
+        void set_formatter(Formatter new_formatter)
+        {
+            formatter_ = new_formatter;
+        }
+
+        void pretty_print()
+        {
+            std::cout << "Text: " << formatter_(data_) << std::endl;
+        }
+
+        std::string data() const
+        {
+            return data_;
+        }
+
+        void set_data(const std::string& data)
+        {
+            data_ = data;
+        }
+    };
+} // namespace ModernCpp
 
 #endif /*STRATEGY_HPP_*/
